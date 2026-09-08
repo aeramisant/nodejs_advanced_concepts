97e0fb4ad8c85b513df30f57fe39c53b6bcc12a5
Alex Eramisant <aeramisant@griddynamics.com>
2026-09-08 13:27:04 +0300

    connetc mongoDB and start initial project

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_DB } = process.env;
 module.exports = {
   googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI:
-    "mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority",
-  cookieKey: "123123123",
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`,
+  cookieKey: '123123123',
 };
