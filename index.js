ac266f793d0f36a39c3de327a8f0f179bb4c3acc
Alex Eramisant <aeramisant@griddynamics.com>
2026-09-16 13:24:15 +0300

    fix failed CI deployment

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
